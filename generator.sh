121f953471ecfa564a0ef396a7fa59d20fb06662
liam8888999 <robsonliam2@gmail.com>
2022-10-15 22:04:18 +1100

    fix random audio generator

--- generator.sh
+++ generator.sh
@@ -28,14 +28,17 @@ find $scriptdir/audio -name '*.mp3' -print > $scriptdir/workdir/music.txt
 awk 'BEGIN{srand()}{print rand(), $0}' $scriptdir/workdir/music.txt | sort -n -k 1 | awk 'sub(/\S* /,"")'
 
 #random number
-
-minimum=1
-maximum=7
-randomNumber=$(($minimum + $RANDOM % $maximum))
+randomNumber=$(shuf -i 1-7 -n 1 --repeat)
+randomNumber1=$(shuf -i 1-7 -n 1 --repeat)
+randomNumber2=$(shuf -i 1-7 -n 1 --repeat)
 audio=$(head -n $randomNumber $scriptdir/workdir/music.txt | tail -n 1)
-
-
+audio1=$(head -n $randomNumber1 $scriptdir/workdir/music.txt | tail -n 1)
+audio2=$(head -n $randomNumber2 $scriptdir/workdir/music.txt | tail -n 1)
 #General cleanup
+
+echo $audio
+echo $audio1
+echo $audio2
 
 rm -f $scriptdir/weather/v1.png
 rm -f $scriptdir/weather/v2.png
@@ -53,6 +56,6 @@ wait
 #make video
 
 ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v1.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v1.mp4
-ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v2.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
-ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v3.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
+ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v2.png -stream_loop -1 -i $audio1 -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
+ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v3.png -stream_loop -1 -i $audio2 -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
 #end weather
