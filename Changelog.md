6798f82afcd7fb268071200adf67f50a40d35d9e
liam8888999 <robsonliam2@gmail.com>
2022-10-15 20:16:40 +1100

    major update and bug fixes

---
+++ Changelog.md
@@ -0,0 +1,27 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+
+## [Unreleased]
+
+### Added
+
+
+
+### Fixed
+
+
+
+## [0.0.1 - Beta] - 2022-10-15
+
+### Added
+
+- support for setting background colour, resolution and length
+- background music
+
+### Fixed
+
+- Bug with spaces in names. the user can now place the name with spaces in the section provided and the script will automatically deal with the spaces as required.
+- Remove blurry background video
--- generator.sh
+++ generator.sh
@@ -4,12 +4,22 @@
 #user definable configuration
 
 #weather
+#type place name inside '' and it will automatically remove the spaces for you
 
-output=/home/xxx/xxx
-city=Melbourne
-state=Victoria
+output=/home/liam/etv-filler
+city='Austin'
+state='Victoria'
+#desired video length e.g. 30 for 30sec -- must be in seconds
+videolength=5
+#desired background colour around image
+backgroundcolour=blue
 
+#advanced user configuration
+#desired video resolution 1280x720
+videoresolution=1280x720
 #non user definable configuration
+
+#setting directory
 
 wdir="$PWD"; [ "$PWD" = "/" ] && wdir=""
 case "$0" in
@@ -17,6 +27,24 @@ case "$0" in
   *) scriptdir="$wdir/${0#./}";;
 esac
 scriptdir="${scriptdir%/*}"
+
+#fix white spaces for curl
+
+stateurl=$(echo $state|sed -e 's/ /%20/g')
+cityurl=$(echo $city|sed -e 's/ /%20/g')
+
+#select audio
+
+find $scriptdir/audio -name '*.mp3' -print > $scriptdir/workdir/music.txt
+
+awk 'BEGIN{srand()}{print rand(), $0}' $scriptdir/workdir/music.txt | sort -n -k 1 | awk 'sub(/\S* /,"")'
+
+#random number
+
+minimum=1
+maximum=7
+randomNumber=$(($minimum + $RANDOM % $maximum))
+audio=$(head -n $randomNumber $scriptdir/workdir/music.txt | tail -n 1)
 
 
 #General cleanup
@@ -29,15 +57,14 @@ rm -f $scriptdir/weather/v3.png
 
 #retrieve weather data
 
-curl wttr.in/${city}_t.png --output $scriptdir/weather/v1.png
-curl v2.wttr.in/${city}_t.png --output $scriptdir/weather/v2.png
-curl v3.wttr.in/${state}_t.png --output $scriptdir/weather/v3.png
+curl wttr.in/${cityurl}.png --output $scriptdir/weather/v1.png
+curl v2.wttr.in/${cityurl}.png --output $scriptdir/weather/v2.png
+curl v3.wttr.in/${stateurl}.png --output $scriptdir/weather/v3.png
 wait
 
 #make video
 
-ffmpeg -i $scriptdir/weather/weather.avi -i $scriptdir/weather/v1.png -filter_complex "[0:v][1:v] overlay=0:50" -pix_fmt yuv420p -c:a copy $output/weather-v1.mp4
-ffmpeg -i $scriptdir/weather/weather.avi -i $scriptdir/weather/v2.png -filter_complex "[0:v][1:v] overlay=200:30" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
-ffmpeg -i $scriptdir/weather/weather.avi -i $scriptdir/weather/v3.png -filter_complex "[0:v][1:v] overlay=4:0" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
-
+ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v1.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v1.mp4
+ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v2.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
+ffmpeg -f lavfi -i color=$backgroundcolour:$videoresolution:d=$videolength -i $scriptdir/weather/v3.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
 #end weather
