5c4db0912630737faaee15b07eaa4de726660d93
liam8888999 <robsonliam2@gmail.com>
2022-10-17 16:57:10 +1100

    commit

--- generator.sh
+++ generator.sh
@@ -54,6 +54,8 @@ newstextcolour=random
 done
 while [[ -z $newsbackgroundcolour ]]; do
 newsbackgroundcolour=random
+else
+echo newsbackgroundcolour=$newsbackgroundcolour >> $workdir/config.conf
 done
 while [[ -z $backgroundcolour ]]; do
 backgroundcolour=random
@@ -139,23 +141,7 @@ weathermeasurement=?u
 else
 weathermeasurement=?m
 fi
-#weather
 
-#retrieve weather data
-
-
-curl wttr.in/${cityurl}.png$weathermeasurement --output $weatherdir/v1.png
-curl v2.wttr.in/${cityurl}.png$weathermeasurement --output $weatherdir/v2.png
-curl v3.wttr.in/${stateurl}.png$weathermeasurement --output $weatherdir/v3.png
-wait
-
-#make video
-
-ffmpeg -y -f lavfi -i color=$background:$videoresolution:d=$videolength -i $weatherdir/v1.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v1.mp4
-ffmpeg -y -f lavfi -i color=$background1:$videoresolution:d=$videolength -i $weatherdir/v2.png -stream_loop -1 -i $audio1 -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
-ffmpeg -y -f lavfi -i color=$background2:$videoresolution:d=$videolength -i $weatherdir/v3.png -stream_loop -1 -i $audio2 -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
-
-#end weather
 
 
 #news
--- helper-scripts/weather.sh
+++ helper-scripts/weather.sh
@@ -2,6 +2,24 @@
 #V0.0.7 - Beta
 
 # load in configuration variables
-. ../config.conf
+. $workdir/config.conf
 
-echo $videolength
+echo $newsbackgroundcolour
+
+#weather
+
+#retrieve weather data
+
+
+curl wttr.in/${cityurl}.png$weathermeasurement --output $weatherdir/v1.png
+curl v2.wttr.in/${cityurl}.png$weathermeasurement --output $weatherdir/v2.png
+curl v3.wttr.in/${stateurl}.png$weathermeasurement --output $weatherdir/v3.png
+wait
+
+#make video
+
+ffmpeg -y -f lavfi -i color=$background:$videoresolution:d=$videolength -i $weatherdir/v1.png -stream_loop -1 -i $audio -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v1.mp4
+ffmpeg -y -f lavfi -i color=$background1:$videoresolution:d=$videolength -i $weatherdir/v2.png -stream_loop -1 -i $audio1 -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v2.mp4
+ffmpeg -y -f lavfi -i color=$background2:$videoresolution:d=$videolength -i $weatherdir/v3.png -stream_loop -1 -i $audio2 -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy $output/weather-v3.mp4
+
+#end weather
