f6c17a0581315d7e4ccf677b18579a4e4fe2afb3
liam8888999 <robsonliam2@gmail.com>
2022-10-17 23:21:34 +1100

    Update autoupdate.sh

--- helper-scripts/autoupdate.sh
+++ helper-scripts/autoupdate.sh
@@ -11,15 +11,17 @@ if [[ $autoupdate1 = yes ]]
 then
 echo '#!/bin/bash' >> /tmp/ErsatzTV-Filler-autoupdate.sh
 echo git pull >> /tmp/ErsatzTV-Filler-autoupdate.sh
-#echo rm $0 >> /tmp/ErsatzTV-Filler-autoupdate.sh
+echo rm $0 >> /tmp/ErsatzTV-Filler-autoupdate.sh
 
-chmod +x /tmp/ErsatzTV-Filler-autoupdate.sh
+cd /tmp
+
+chmod +x ErsatzTV-Filler-autoupdate.sh
 
 # cleanup
 
 rm -f $helperdir/config-temp.conf
 
-exec /tmp/ErsatzTV-Filler-autoupdate.sh
+exec /ErsatzTV-Filler-autoupdate.sh
 else
 rm -f $helperdir/config-temp.conf
 exit 0
