2d399ef866be902944cf1c57b992a1123c3adb58
liam8888999 <robsonliam2@gmail.com>
2022-10-21 16:49:58 +1100

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -291,13 +291,11 @@ fi
 #add number to begining of line for randomisation
 awk 'BEGIN{srand()}{print rand(), $0}' $workdir/music.txt | sort -n -k 1 | awk 'sub(/\S* /,"")'
 audioamount=$(wc -l $workdir/music.txt)
-audionumber=$(
 if [ $audioamount == 1 ]; then
 audionumber=1
 else
 audionumber=echo "$(shuf -i 1-$audioamount -n 1 --repeat))"
 fi
-)
 
 #End Audio
 
