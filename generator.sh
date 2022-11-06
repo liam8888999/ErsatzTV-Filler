89a9c771c3b90dc2121841ae6acf38d7b81f1a2e
liam8888999 <robsonliam2@gmail.com>
2022-11-07 01:46:11 +1100

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -8,14 +8,14 @@ if [ ! -d /tmp/ErsatzTV-Filler ]; then
 fi
 version="V0.0.17 - Beta"
 #exec 1>>$script_log
-exec > >(tee -a $log_location/log_`date +%F`.log) 2>&1
+exec > >(tee -a "$log_location/log_`date +%F`.log") 2>&1
 echo ""
 echo '-----------------------------------------------------------------------------------------------'
 echo ""
 date
 version="V0.0.17 - Beta"
 echo $version
-echo this will automatically output to a log file at $log_location/log_`date +%F`.log
+echo this will automatically output to a log file at "$log_location/log_`date +%F`.log"
 
 #finish logging
 
