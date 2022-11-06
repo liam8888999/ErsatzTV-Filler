ee1835dd3cee182fc0cd8472d4cc3dd2917c1f88
liam8888999 <robsonliam2@gmail.com>
2022-11-06 20:16:44 +1100

    Update weather.sh

--- helper-scripts/weather.sh
+++ helper-scripts/weather.sh
@@ -67,11 +67,11 @@ wait
 
 #make video
 
-ffmpeg -y -f lavfi -i color=$background:$videoresolution -i $weatherdir/v1.png -stream_loop -1 -i "$audio" -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2;fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v1.mp4
+ffmpeg -y -f lavfi -i color=$background:$videoresolution -i $weatherdir/v1.png -stream_loop -1 -i "$audio" -shortest -filter_complex "fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6;[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v1.mp4
 touch $output/weather-v1.mp4
-ffmpeg -y -f lavfi -i color=$background1:$videoresolution -i $weatherdir/v2.png -stream_loop -1 -i "$audio1" -shortest -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2;fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v2.mp4
+ffmpeg -y -f lavfi -i color=$background1:$videoresolution -i $weatherdir/v2.png -stream_loop -1 -i "$audio1" -shortest -filter_complex "fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6;[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v2.mp4
 touch $output/weather-v2.mp4
-ffmpeg -y -f lavfi -i color=$background2:$videoresolution -i $weatherdir/v3.png -stream_loop -1 -i "$audio2" -shortest -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2;fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v3.mp4
+ffmpeg -y -f lavfi -i color=$background2:$videoresolution -i $weatherdir/v3.png -stream_loop -1 -i "$audio2" -shortest -filter_complex "fade=type=out:duration=2:start_time=6;afade=type=out:duration=2:start_time=6;[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy -t $videolength $output/weather-v3.mp4
 touch $output/weather-v3.mp4
 #end weather
 ./news.sh
