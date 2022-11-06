d35f671c165a496f78b01d2e4aea2b1b973b55a0
liam8888999 <robsonliam2@gmail.com>
2022-11-06 23:47:41 +1100

    Update autoupdate.sh

--- helper-scripts/autoupdate.sh
+++ helper-scripts/autoupdate.sh
@@ -29,10 +29,10 @@ echo date >> /tmp/ErsatzTV-Filler-autoupdate.sh
 echo 'echo updating' >> /tmp/ErsatzTV-Filler-autoupdate.sh
 echo 'exec 2>&1' >> /tmp/ErsatzTV-Filler-autoupdate.sh
 echo cd $scriptdir >> /tmp/ErsatzTV-Filler-autoupdate.sh
-#echo git pull >> /tmp/ErsatzTV-Filler-autoupdate.sh
+echo git pull >> /tmp/ErsatzTV-Filler-autoupdate.sh
 echo rm \$0 >> /tmp/ErsatzTV-Filler-autoupdate.sh
 rm -f $scriptdir/running.txt >> /tmp/ErsatzTV-Filler-autoupdate.sh
-#echo .$scriptdir/generator.sh >> /tmp/ErsatzTV-Filler-autoupdate.sh
+echo exec $scriptdir/generator.sh >> /tmp/ErsatzTV-Filler-autoupdate.sh
 
 chmod +x /tmp/ErsatzTV-Filler-autoupdate.sh
 
@@ -48,4 +48,3 @@ rm -f $scriptdir/running.txt
 cd $scriptdir
 #./generator.sh
 fi
-c
