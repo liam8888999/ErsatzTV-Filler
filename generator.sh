5e5dcdda23ecea4202924aa96b72ca74b4f14e42
liam8888999 <robsonliam2@gmail.com>
2022-11-07 01:16:30 +1100

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -7,13 +7,12 @@ script_log="/tmp/ErsatzTV-Filler/log_`date +%F`.log"
 version="V0.0.17 - Beta"
 echo $version
 echo this will automatically output to a log file at $script_log
-exec 3>&1
-exec 1>>$script_log
 echo ""
 echo '-----------------------------------------------------------------------------------------------'
 echo ""
 date
 exec 2>&1
+exec 1>>$script_log
 
 rm -f /tmp/ErsatzTV-Filler-autoupdate.sh
 
