eb6105019e6ca972cb50b1ed78da49e09fe47f4a
liam8888999 <robsonliam2@gmail.com>
2022-11-20 18:38:44 +1100

    Update weather.sh

--- helper-scripts/weather.sh
+++ helper-scripts/weather.sh
@@ -123,7 +123,7 @@ then
   ffmpeg -y -f lavfi -i color=$background1:$videoresolution -i $weatherdir/v2.png -filter_complex "[1]scale=iw*1:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy -t $videolength $workdir/weatherv4/v2.mp4
   ffmpeg -y -f lavfi -i color=$background2:$videoresolution -i $weatherdir/v3.png -filter_complex "[1]scale=iw*0.9:-1[wm];[0][wm]overlay=x=(W-w)/2:y=(H-h)/2" -pix_fmt yuv420p -c:a copy -t $videolength $workdir/weatherv4/v3.mp4
   cd $workdir/weatherv4
-  for f in *.mp4; do echo "file '$f'" >> $workdir/v4.txt; done
+  for f in *.mp4; do echo "file '$workdir/weatherv4/$f'" >> $workdir/v4.txt; done
   cd $helperdir
   cat $workdir/v4.txt
   ffmpeg -y -f concat -i $workdir/v4.txt -c copy $workdir/weather-v4.mp4
