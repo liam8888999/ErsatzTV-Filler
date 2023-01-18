006fbaa084260ccd7b76e4f38adcad443dfc93e4
NotExpectedYet <online@notexpectedyet.com>
2023-01-18 06:29:54 +0000

    add ejs and ejs layout dependencies and use in web.js

--- web.js
+++ web.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const fs = require('fs');
+
+// Setup express middleware for ejs view engine, allows the use
+// of layouts and ejs templating for html generation on the server
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/config.html');
 });
