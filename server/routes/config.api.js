f94b94ccd1236e03243d1c372a64f8f259b16f47
James <james@vexica.com>
2023-01-19 14:16:27 +0000

    fix borked api endpoint

--- server/routes/config.api.js
+++ server/routes/config.api.js
@@ -6,7 +6,10 @@ const loadApiRoutes = (app) => {
      * Patch route to receive updates to the configuration file.
      */
     app.patch(ROUTE_CONSTANTS().CONFIG_ROUTE_EDIT, async (req, res) => {
-        await writeValueToConfigurationFile(req.body)
+        for (const [key, value] of Object.entries(req.body)) {
+            await writeValueToConfigurationFile(key, value)
+        }
+
         res.send({ result: "success" })
     });
 
