abc41d1020393a4e2d09c236d08fe863ec16d4a7
liam8888999 <robsonliam2@gmail.com>
2023-01-20 17:20:09 +1100

    Update randomaudio.utils.js

--- server/generators/utils/randomaudio.utils.js
+++ server/generators/utils/randomaudio.utils.js
@@ -2,10 +2,9 @@ const { listFilesindir } = require("../../utils/file.utils")
 const { randomNumber } = require("../../utils/randomnumber.utils")
 
 function randomaudio(){
-const fileList = await listFiles('${retrieveCurrentConfiguration().customaudio}')
 const selectRandomAudioFile = async (path) => {
   //gra the array of files
-  const fileList = await listFiles(path)
+  const fileList = await listFiles('${retrieveCurrentConfiguration().customaudio}')
   //generate a random number from array 0 to length
   const randomIndex = randomNumber(fileList.length);
 
