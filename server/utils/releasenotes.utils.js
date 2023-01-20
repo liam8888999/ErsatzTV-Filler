1a51db41504c06c2e3e6c7b55ddd853e8d84aac6
liam8888999 <robsonliam2@gmail.com>
2023-01-20 23:30:55 +1100

    Update releasenotes.utils.js

--- server/utils/releasenotes.utils.js
+++ server/utils/releasenotes.utils.js
@@ -1,5 +1,5 @@
-const { readFile } = require('fs/promises')
-
+const fs = require('fs');
+let data;
 fs.readFile('releasenotes.utils.js', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
