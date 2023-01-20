e7b7d37d5e2f0f64cf8679f5f3c8864dda5b1049
liam8888999 <robsonliam2@gmail.com>
2023-01-21 00:24:07 +1100

    commit

--- server/utils/releasenotes.utils.js
+++ server/utils/releasenotes.utils.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
-let data;
+const marked = require('marked');
+const ejs = require('ejs');
+
 fs.readFile('../../Changelog.md', 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+    if (err) throw err;
+
+    const html = marked(data);
+    ejs.renderFile('home.ejs', {markdown: html}, (err, str) => {
+        if (err) throw err;
+        console.log(str);
+    });
 });
