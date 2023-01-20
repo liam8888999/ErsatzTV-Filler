669385c72667ec2e5152b56603e6876aad1047f2
liam8888999 <robsonliam2@gmail.com>
2023-01-20 19:47:00 +1100

    Update downloadimage.utils.js

--- server/utils/downloadimage.utils.js
+++ server/utils/downloadimage.utils.js
@@ -1,6 +1,4 @@
-const http = require('http');
-const client = http.createClient(80, 'example.com');
-
+const axios = require('axios');
 function downloadImage(url, filepath) {
     return new Promise((resolve, reject) => {
         client.get(url, (res) => {
