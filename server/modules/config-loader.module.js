e5dd8386bcc8591b8f76ab1770e77abe92af0d1d
liam8888999 <robsonliam2@gmail.com>
2023-01-20 22:39:09 +1100

    commit

--- server/modules/config-loader.module.js
+++ server/modules/config-loader.module.js
@@ -28,6 +28,7 @@ const setupConfigurationFile = async () => {
  */
 const parseConfigurationFileContents = async (path) => {
     CURRENT_CONFIG = parseConfigurationFile(path).parsed;
+    console.log(parseConfigurationFile(path).parsed)
 }
 
 /**
