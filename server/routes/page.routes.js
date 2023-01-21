440c83fbaa2b3dd4e14eed2025157da3cc8d63f3
liam8888999 <robsonliam2@gmail.com>
2023-01-22 00:00:58 +1100

    commit

--- server/routes/page.routes.js
+++ server/routes/page.routes.js
@@ -3,14 +3,19 @@ const { retrieveCurrentConfiguration } = require("../modules/config-loader.modul
 const { generateChangelog } = require("../utils/markdown.utils")
 
 const loadPageRoutes = (app) => {
-    app.get('/', async (req, res) => {
-  const html = await generateChangelog()
+  app.get('/', async (req, res) => {
+    let html = await generateChangelog()
+    console.log("original html")
     console.log(html)
-    res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "home", {
-      markdown: html,
-      layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
-      page: "Home" //This is used by the front end to figure out where it is, allows us to statically set the active class on the navigation links. The page will not load without this variable.
- });
+  html = html.replace(/<p>All notable changes to this project will be documented in this file.<\/p>/g, "").replace(/<p>The format is based on <a href=.*>Keep a Changelog<\/a>,<\/p>/g, "");
+    console.log("changed html")
+      console.log(html)
+      res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "home", {
+        markdown: html,
+        layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
+        page: "Home" //This is used by the front end to figure out where it is, allows us to statically set the active class on the navigation links. The page will not load without this variable.
+   });
+
   });
 
     app.get('/config', (req, res) => {
