bd33a200f62991f592ec5e4f30d8f8e8909cfbc0
liam8888999 <robsonliam2@gmail.com>
2023-05-31 00:59:04 +1000

    added: ersatzttv quick link now uses xmltv url from config.json

--- server/routes/page.routes.js
+++ server/routes/page.routes.js
@@ -12,53 +12,70 @@ const loadPageRoutes = (app) => {
   const parent = $('h3').eq(1).parent();
   const content = parent.find('h3').eq(1).addClass('expand-button').nextAll();
   content.addBack().wrapAll('<div class="expand-content hidden"></div>');
+  let config_current = await retrieveCurrentConfiguration()
+  let ersatz = config_current.xmltv
+  let ErsatzTVURL = ersatz.replace("/iptv/xmltv.xml", "");
       res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "home", {
         markdown: $.html(),
         layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
         page: "Home", //This is used by the front end to figure out where it is, allows us to statically set the active class on the navigation links. The page will not load without this variable.
-        version: version
+        version: version,
+        ErsatzTVURL: ErsatzTVURL
    });
 
   });
 
     app.get('/config', async (req, res) => {
       let config_current = await retrieveCurrentConfiguration()
+      let ersatz = config_current.xmltv
+      let ErsatzTVURL = ersatz.replace("/iptv/xmltv.xml", "");
         // Render the specific ejs template view
         res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "config", {
             layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
             page: "Config",
             CURRENT_CONFIG: config_current,
-            version: version //sending the current configuration to the ejs template.
+            version: version, //sending the current configuration to the ejs template.
+            ErsatzTVURL: ErsatzTVURL
         });
     });
 
     app.get('/themes', async (req, res) => {
       let config_current = await retrieveCurrentConfiguration()
+      let ersatz = config_current.xmltv
+      let ErsatzTVURL = ersatz.replace("/iptv/xmltv.xml", "");
         res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "themes", {
             layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
             page: "Themes",
             version: version,
-            theme: config_current.theme
+            theme: config_current.theme,
+            ErsatzTVURL: ErsatzTVURL
         });
     });
 
-    app.get('/updates', (req, res) => {
+    app.get('/updates', async (req, res) => {
+      let config_current = await retrieveCurrentConfiguration()
+      let ersatz = config_current.xmltv
+      let ErsatzTVURL = ersatz.replace("/iptv/xmltv.xml", "");
         // Render the specific ejs template view
         res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "update", {
             layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
             page: "Updates",
-            version: version
+            version: version,
+            ErsatzTVURL: ErsatzTVURL
         });
     });
 
     app.get('/documentation', async (req, res) => {
 let documentation = await generateReadMe()
-
-        res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "documentation", {
+let config_current = await retrieveCurrentConfiguration()
+let ersatz = config_current.xmltv
+let ErsatzTVURL = ersatz.replace("/iptv/xmltv.xml", "");
+  res.render(TEMPLATE_CONSTANTS().PAGES_FOLDER + "documentation", {
           documentation: documentation,
           layout: TEMPLATE_CONSTANTS().DEFAULT_LAYOUT, //Just registering which layout to use for each view
           page: "Documentation", //This is used by the front end to figure out where it is, allows us to statically set the active class on the navigation links. The page will not load without this variable.
-          version: version
+          version: version,
+          ErsatzTVURL: ErsatzTVURL
      });
 
     });
