49593b3fc347548e0fe7923e7e005a8c5edbc657
liam8888999 <robsonliam2@gmail.com>
2023-06-16 02:12:39 +1000

    fix: set all subtitles as 1 large block

--- server/generators/news.generator.js
+++ server/generators/news.generator.js
@@ -89,38 +89,43 @@ const news1Content = newstempContent
       const inputText = fs.readFileSync(`${WORKDIR}/news-temp.txt`, 'utf8');
 
       // Split the inputText into separate lines
-      const lines = inputText.split('\n');
+    //  const lines = inputText.split('\n');
+
+    // Replace line breaks with \N
+const lines = inputText.replace(/\n/g, '\\N');
+console.log(lines)
 
       // Calculate the duration for each subtitle
-      const subtitleDuration = 2; // Duration in seconds
+      const subtitleDuration = 4; // Duration in seconds
 
       // Calculate the start and end time for each subtitle
-      let startTime = 2; // Start time adjusted by 2 seconds
-      let endTime = startTime + subtitleDuration;
+      let startTime = 1; // Start time adjusted by 2 seconds
+      let endTime = 90 + subtitleDuration;
       let assText = `[Script Info]
-      Title: Scrolling Text Example
-      ScriptType: v4.00+
-      WrapStyle: 0
-      PlayResX: 1280
-      PlayResY: 720
+Title: Scrolling Text Example
+ScriptType: v4.00+
+WrapStyle: 0
+PlayResX: 1280
+PlayResY: 720
 
-      [V4+ Styles]
-      Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-      Style: Default, Arial, 30, &H00FFFFFF, &H00000000, &H00000000, &H00000000, 0, 0, 0, 0, 100, 100, 0, 0, 1, 1, 1, 2, 30, 30, 30, 0
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default, Arial, 32, &H00000000, &H00000000, &H00000000, &H00000000, 0, 0, 0, 0, 100, 100, 0, 0, 0, 0, 0, 2, 30, 30, 30, 0
 
-      [Events]
-      Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text`;
-
-      for (let i = 0; i < lines.length; i++) {
-        const line = lines[i];
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+Dialogue: 0, 0:00:${startTime.toString().padStart(2, '0')}.00, 0:00:${endTime.toString().padStart(2, '0')}.00, Default, ScrollText, 0, 0, 0, ,{\\move(640,-120,640,0)}${lines}`
+;
+    //  for (let i = 0; i < lines.length; i++) {
+      //  const line = lines[i];
 
         // Add the start and end time for each subtitle
-        assText += `\nDialogue: 0, 0:00:${startTime.toString().padStart(2, '0')}.00, 0:00:${endTime.toString().padStart(2, '0')}.00, Default, ScrollText, 0, 0, 0, ,{\\move(640,720,640,0)}${line}`;
+        //assText += `\nDialogue: 0, 0:00:${startTime.toString().padStart(2, '0')}.00, 0:00:${endTime.toString().padStart(2, '0')}.00, Default, ScrollText, 0, 0, 0, ,{\\move(640,720,640,0)}${line}`;
 
         // Increment the start and end time for the next subtitle
-        startTime += subtitleDuration;
-        endTime += subtitleDuration;
-      }
+        //startTime += subtitleDuration;
+        //endTime += subtitleDuration;
+    //  }
 
 
       // Save the ASS text to a file
